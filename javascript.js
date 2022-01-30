cc1b40537cba615e1f0e13af54b75fee7a26edef
CNPCNPCNP <clayh_94@hotmail.com>
2022-01-30 13:03:04 +1000

    Complete game functionality

--- javascript.js
+++ javascript.js
@@ -1,16 +1,54 @@
 function computerPlay() {
     let randomInt = Math.random() * 3
     if (randomInt < 1) {
-        return 'Paper'
+        return 'paper'
     }
     else if (randomInt < 2) {
-        return 'Scissors'
+        return 'scissors'
     }
     else {
-        return 'Rock'
+        return 'rock'
     }
 }
 
-console.log(computerPlay())
-console.log(computerPlay())
-console.log(computerPlay())
+function playRound(playerSelection, computerSelection) {
+    playerSelection = playerSelection.toLowerCase()
+    switch(playerSelection) {
+        case 'rock':
+            switch(computerSelection) {
+                case 'rock':
+                    return 'Draw'
+                case 'paper':
+                    return 'Loser! Loser!! haha'
+                case 'scissors':
+                    return 'Winner is you!'
+            }
+        case 'paper':
+            switch(computerSelection) {
+                case 'rock':
+                    return 'Winner is you!'
+                case 'paper':
+                    return 'Draw'
+                case 'scissors':
+                    return 'Loser! Loser!! haha'
+            }
+        case 'scissors':
+            switch(computerSelection) {
+                case 'rock':
+                    return 'Loser! Loser!! haha'
+                case 'paper':
+                    return 'Winner is you!'
+                case 'scissors':
+                    return 'Draw'
+            }
+    }
+}
+
+function game() {
+    for (let i = 0; i < 5; i++) {
+        let playerSelection = prompt('Rock, paper or scissors?')
+        console.log(playRound(playerSelection, computerPlay()))
+    }
+}
+
+game()
